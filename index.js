e8b2a9a10e96d12828b130220f85189f582c5ce8
myoan <motoki.yoan@gmail.com>
2017-01-14 22:39:09 +0900

    Add each action class

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function IssueComment(msg) {
   this.pretext     = pretextHeader(this.repoUrl, this.repoName)
                        + link(this.authorUrl, this.authorName)
                        + " comments on: "
-                       + prTitleWithNumber(this.titleUrl, this.prNum, this.title)
+                       + prTitleWithNumber(this.titleUrl, this.prNum, this.title);
   this.text     = msg.comment.body
 }
 
@@ -63,7 +63,7 @@ function PRReview(msg) {
   this.pretext     = pretextHeader(this.repoUrl, this.repoName)
                        + link(this.authorUrl, this.authorName)
                        + " reviews on: "
-                       + prTitleWithNumber(this.titleUrl, this.prNum, this.title)
+                       + prTitleWithNumber(this.titleUrl, this.prNum, this.title);
   this.text     = + "[*" + msg.review.state + "*] " + msg.comment.body
 }
 
@@ -85,16 +85,16 @@ PRReview.prototype.toSlack = function() {
 function PRPush(msg) {
   this.repoName   = msg.repository.full_name;
   this.repoUrl    = msg.repository.html_url;
-  this.authorName = msg.sender.login;
-  this.authorUrl  = msg.sender.html_url;
+  this.authorName = msg.issue.user.login;
+  this.authorUrl  = msg.issue.user.html_url;
+  this.color      = "#36a64f";
+  this.pretext    = pretextHeader(this.repoUrl, this.repoName)
+                      + link(this.authorUrl, this.authorName);
+  this.text       = "";
   for (var i = 0; i < msg.commits.length; i++) {
     var commit = msg.commits[i];
     this.text += link(commit.url, commit.id.substr(0, 8)) + ' ' + commit.message + ' - ' + commit.author.name + "\n";
   }
-  this.color       = "#36a64f";
-  this.pretext     = pretextHeader(this.repoUrl, this.repoName)
-                       + link(this.authorUrl, this.authorName)
-  this.text     = + "[*" + msg.review.state + "*] " + msg.comment.body
 }
 
 PRPush.prototype.toSlack = function() {
@@ -112,8 +112,70 @@ PRPush.prototype.toSlack = function() {
   }
 };
 
+function Issue(msg) {
+  this.repoName   = msg.repository.full_name;
+  this.repoUrl    = msg.repository.html_url;
+  this.authorName = msg.sender.login;
+  this.authorUrl  = msg.sender.html_url;
+  this.title      = msg.issue.title;
+  this.titleUrl   = msg.issue.html_url;
+  this.prNum      = msg.issue.number;
+  this.text       = msg.issue.body;
+  this.color      = "#36a64f";
+  this.pretext    = pretextHeader(this.repoUrl, this.repoName)
+                      + link(this.authorUrl, this.authorName)
+                      + " creates issue: "
+                      + prTitleWithNumber(this.titleUrl, this.prNum, this.title);
+}
+
+Issue.prototype.toSlack = function() {
+  return {
+    attachments: [
+      {
+        fallback: "Required plain-text summary of the attachment.",
+        color: this.color,
+        pretext: this.pretext,
+        text: this.text,
+        image_url: "http://my-website.com/path/to/image.jpg",
+        thumb_url: "http://example.com/path/to/thumb.png"
+      }
+    ]
+  }
+};
+
+function PullRequest(msg) {
+  this.repoName   = msg.repository.full_name;
+  this.repoUrl    = msg.repository.html_url;
+  this.authorName = msg.pull_request.user.login;
+  this.authorUrl  = msg.pull_request.user.html_url;
+  this.title      = msg.pull_request.title;
+  this.titleUrl   = msg.pull_request.html_url;
+  this.prNum      = msg.pull_request.number;
+  this.text       = msg.pull_request.body;
+  this.color      = "#36a64f";
+  this.pretext    = pretextHeader(this.repoUrl, this.repoName)
+                      + link(this.authorUrl, this.authorName)
+                      + " creates PR: "
+                      + prTitleWithNumber(this.titleUrl, this.prNum, this.title);
+}
+
+PullRequest.prototype.toSlack = function() {
+  return {
+    attachments: [
+      {
+        fallback: "Required plain-text summary of the attachment.",
+        color: this.color,
+        pretext: this.pretext,
+        text: this.text,
+        image_url: "http://my-website.com/path/to/image.jpg",
+        thumb_url: "http://example.com/path/to/thumb.png"
+      }
+    ]
+  }
+};
+
 exports.handler = function (event, context) {
-  console.log('Received GitHub event: ' + event.Records[0].Sns.Message);
+  // console.log('Received GitHub event: ' + event.Records[0].Sns.Message);
   var msg = JSON.parse(event.Records[0].Sns.Message);
   var eventName = event.Records[0].Sns.MessageAttributes['X-Github-Event'].Value;
   var text = '';
@@ -138,40 +200,13 @@ exports.handler = function (event, context) {
       action = new PRReview(msg)
       break;
     case 'issues':
-      action = new PRReview(msg)
-      // var issue = msg.issue;
-      // authorName = issue.user.login;
-      // authorUrl  = issue.user.html_url;
-      // prTitle    = msg.issue.title;
-      // prUrl      = msg.issue.html_url;
-      // prNum      = msg.issue.number;
-      // if (msg.action == 'opended' || msg.action == 'closed' || msg.action == 'reopened') {
-      //     text += 'Issue ' + msg.action + "\n";
-      //     text += link(issue.html_url, issue.title);
-      // }
+      action = new Issue(msg)
       break;
     case 'push':
       action = new PRPush(msg)
-      // authorName = msg.sender.login;
-      // authorUrl  = msg.sender.html_url;
-      // text += 'Pushed' + "\n";
-      // text += msg.compare + "\n";
-      // for (var i = 0; i < msg.commits.length; i++) {
-      //   var commit = msg.commits[i];
-      //   text += link(commit.url, commit.id.substr(0, 8)) + ' ' + commit.message + ' - ' + commit.author.name + "\n";
-      // }
       break;
     case 'pull_request':
-      action = new PRReview(msg)
-      // var pull_request = msg.pull_request;
-      // authorName = pull_request.user.login;
-      // authorUrl  = pull_request.user.html_url;
-      // prTitle    = pull_request.title;
-      // prUrl      = pull_request.html_url;
-      // prNum      = pull_request.number;
-      // if (msg.action == 'opended' || msg.action == 'closed' || msg.action == 'reopened') {
-      //     text += 'Pull Request ' + msg.action + "\n";
-      // }
+      action = new PullRequest(msg)
       break;
   }
 
@@ -180,22 +215,6 @@ exports.handler = function (event, context) {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
     json: action.toSlack(),
-    // json: {
-    //   attachments: [
-    //     {
-    //       fallback: "Required plain-text summary of the attachment.",
-    //       color: "#36a64f",
-    //       pretext: "[<" + repoUrl + "|" + repoName +">]",
-    //       author_name: authorName,
-    //       author_link: authorUrl,
-    //       title: "#" + prNum + ": " + prTitle,
-    //       title_link: prUrl,
-    //       text: text,
-    //       image_url: "http://my-website.com/path/to/image.jpg",
-    //       thumb_url: "http://example.com/path/to/thumb.png"
-    //     }
-    //   ]
-    // }
   }, function () {
     context.done();
   });
---
+++ test/index.test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var fs = require('fs');
+var expect = require('chai').expect;
+var LambdaTester = require('lambda-tester');
+var github2slack = require('../index');
+
+describe( 'github2slack', function() {
+  [
+    'test/input/issue_comment.json',
+    'test/input/push.json',
+    'test/input/issue.json',
+    'test/input/pull_request.json'
+  ].forEach( function(filePath) {
+    it( 'successful test',
+      function(done) {
+        LambdaTester(github2slack.handler)
+          .event( JSON.parse(fs.readFileSync(filePath)))
+          .expectSucceed( function( result ) {
+            console.log(result);
+            expect( result ).to.be.null;
+          })
+          .verify( done );
+      }
+    );
+  });
+});
